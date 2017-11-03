c3c27f38173aac711c68be9ad92ba9d0e79feb0f
cuiyuqiang@juzix.io <1qazxsw23edc>
2017-11-03 13:31:29 +0800

    自定义列名处理(尽量规范命名)

--- generatorcode-operationplatform/src/main/java/com/github/fartherp/generatorcode/op/OpGenerator.java
+++ generatorcode-operationplatform/src/main/java/com/github/fartherp/generatorcode/op/OpGenerator.java
@@ -30,13 +30,35 @@ public class OpGenerator extends MyBatisGenerator<OpAttributes> {
 
     @Override
     public String dealTableName(String tableName) {
-        String[] array = tableName.replaceFirst("tb_", "")
-                .replaceFirst("td_", "")
-                .replaceFirst("t_", "")
-                .split("_");
+        if (tableName.indexOf("tb_") == 0) {
+            tableName = tableName.replaceFirst("tb_", "");
+        } else if (tableName.indexOf("td_") == 0) {
+            tableName = tableName.replaceFirst("td_", "");
+        } else if (tableName.indexOf("t_") == 0) {
+            tableName = tableName.replaceFirst("t_", "");
+        }
+        String[] array = tableName.split("_");
         StringBuilder s = new StringBuilder();
         for (String a : array) {
             s.append(a.substring(0, 1).toUpperCase() + a.substring(1, a.length()));
+        }
+
+        return s.toString();
+    }
+
+    public String dealColumnName(String columnName) {
+        if (columnName.indexOf("n_") == 0) {
+            columnName = columnName.replaceFirst("n_", "");
+        } else if (columnName.indexOf("c_") == 0) {
+            columnName = columnName.replaceFirst("c_", "");
+        } else if (columnName.indexOf("t_") == 0) {
+            columnName = columnName.replaceFirst("t_", "");
+        }
+        String[] array = columnName.split("_");
+        StringBuilder s = new StringBuilder();
+        s.append(array[0]);
+        for (int i = 1; i < array.length; i++) {
+            s.append(array[i].substring(0, 1).toUpperCase() + array[i].substring(1, array[i].length()));
         }
 
         return s.toString();
