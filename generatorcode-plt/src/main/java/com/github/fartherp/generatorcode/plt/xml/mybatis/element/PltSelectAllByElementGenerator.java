80ad78acbeb389a8df36cf4abbe46d37bf767778
cuiyuqiang@juzix.io <1qazxsw23edc>
2018-04-10 14:40:32 +0800

    添加basexml

--- generatorcode-plt/src/main/java/com/github/fartherp/generatorcode/plt/xml/mybatis/element/PltSelectAllByElementGenerator.java
+++ generatorcode-plt/src/main/java/com/github/fartherp/generatorcode/plt/xml/mybatis/element/PltSelectAllByElementGenerator.java
@@ -6,7 +6,9 @@ package com.github.fartherp.generatorcode.plt.xml.mybatis.element;
 
 import com.github.fartherp.codegenerator.xml.mybatis.element.AbstractXmlElementGenerator;
 import com.github.fartherp.generatorcode.plt.db.PltAttributes;
+import com.github.fartherp.javaxml.Attribute;
 import com.github.fartherp.javaxml.TextElement;
+import com.github.fartherp.javaxml.XmlElement;
 
 /**
  * Created by IntelliJ IDEA.
@@ -40,5 +42,11 @@ public class PltSelectAllByElementGenerator extends AbstractXmlElementGenerator<
         sb.append("from ");
         sb.append(tableInfoWrapper.getTableInfo().getAliasedFullyQualifiedTableNameAtRuntime());
         answer.addElement(new TextElement(sb.toString()));
+    }
+
+    protected XmlElement getBaseColumnListElement() {
+        XmlElement answer = new XmlElement("include");
+        answer.addAttribute(new Attribute("refid", attributes.getDao().getShortName() + "_" + tableInfoWrapper.getAttributes().getBaseColumnList()));
+        return answer;
     }
 }
